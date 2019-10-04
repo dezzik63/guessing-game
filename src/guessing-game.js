ab60d1f15be0ad857406b82d3ad649bf82122525
dezzik63 <artem5553@mail.ru>
2019-10-04 18:54:35 +0300

    guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.min1=min;
+        this.max1=max;
+        this.medium = 0;
     }
 
     guess() {
-
+        this.medium = Math.ceil((this.min1+this.max1)/2);
+        return this.medium;
     }
 
     lower() {
-
+        this.max1 = this.medium
     }
 
     greater() {
-
+        this.min1 = this.medium 
     }
 }
 
